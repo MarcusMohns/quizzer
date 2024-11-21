8cf80d4ff6e2ff5a7d691093a2e132c924f84b05
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2024-11-21 17:00:38 +0100

    cleanup and move Controls to seperate file

---
+++ src/QuizControls.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+const QuizControls = ({
+  activeStep,
+  setActiveStep,
+  setCompleted,
+  completed,
+  steps,
+}) => {
+  const totalSteps = () => {
+    return steps.length;
+  };
+
+  const completedSteps = () => {
+    return Object.keys(completed).length;
+  };
+
+  const isLastStep = () => {
+    return activeStep === totalSteps() - 1;
+  };
+
+  const handleComplete = () => {
+    setCompleted({
+      ...completed,
+      [activeStep]: true,
+    });
+    handleNext();
+  };
+
+  const allStepsCompleted = () => {
+    return completedSteps() === totalSteps();
+  };
+
+  const handleNext = () => {
+    const newActiveStep =
+      isLastStep() && !allStepsCompleted()
+        ? // It's the last step, but not all steps have been completed,
+          // find the first step that has been completed
+          steps.findIndex((step, i) => !(i in completed))
+        : activeStep + 1;
+    setActiveStep(newActiveStep);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+    setCompleted({});
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        p: 2,
+        alignSelf: "flex-end",
+        mt: "auto",
+        width: "100%",
+      }}
+    >
+      {allStepsCompleted() ? (
+        <React.Fragment>
+          <Typography>All steps completed - you&apos;re finished</Typography>
+          <Box>
+            <Box />
+            <Button onClick={handleReset}>Reset</Button>
+          </Box>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <Typography sx={{ mr: "auto" }}>Step {activeStep + 1}</Typography>
+          <Box>
+            <Button
+              color="inherit"
+              disabled={activeStep === 0}
+              onClick={handleBack}
+            >
+              Back
+            </Button>
+            <Button onClick={handleNext}>Next</Button>
+            {activeStep !== steps.length &&
+              (completed[activeStep] ? (
+                <Typography variant="caption" sx={{ display: "inline-block" }}>
+                  Step {activeStep + 1} already completed
+                </Typography>
+              ) : (
+                <Button onClick={handleComplete}>
+                  {completedSteps() === totalSteps() - 1
+                    ? "Finish"
+                    : "Complete Step"}
+                </Button>
+              ))}
+          </Box>
+        </React.Fragment>
+      )}
+    </Box>
+  );
+};
+
+export default QuizControls;
--- src/QuizStepper.jsx
+++ src/QuizStepper.jsx
@@ -3,7 +3,6 @@ import Box from "@mui/material/Box";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepButton from "@mui/material/StepButton";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Tooltip from "@mui/material/Tooltip";
 
@@ -12,85 +11,33 @@ export default function QuizStepper({
   activeStep,
   setActiveStep,
   completed,
-  setCompleted,
 }) {
-  const totalSteps = () => {
-    return steps.length;
-  };
-
-  const completedSteps = () => {
-    return Object.keys(completed).length;
-  };
-
-  const isLastStep = () => {
-    return activeStep === totalSteps() - 1;
-  };
-
-  const allStepsCompleted = () => {
-    return completedSteps() === totalSteps();
-  };
-
-  const handleNext = () => {
-    const newActiveStep =
-      isLastStep() && !allStepsCompleted()
-        ? // It's the last step, but not all steps have been completed,
-          // find the first step that has been completed
-          steps.findIndex((step, i) => !(i in completed))
-        : activeStep + 1;
-    setActiveStep(newActiveStep);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
   const handleStep = (step) => () => {
     setActiveStep(step);
-  };
-
-  const handleComplete = () => {
-    setCompleted({
-      ...completed,
-      [activeStep]: true,
-    });
-    handleNext();
-  };
-
-  const handleReset = () => {
-    setActiveStep(0);
-    setCompleted({});
   };
 
   return (
     <Box
       sx={{
-        display: "flex",
-        flexWrap: "wrap",
-        height: "500px",
-        p: 5,
+        p: 2,
       }}
     >
       <Stepper
         nonLinear
-        alternativeLabel
         activeStep={activeStep}
-        sx={{ flexWrap: "wrap" }}
+        sx={{ justifyContent: "center", flexWrap: "wrap" }}
       >
         {steps.map((label, index) => (
-          <Tooltip title={label.question}>
-            <Step
-              key={label.question}
-              completed={completed[index]}
-              sx={{ width: "100px" }}
-            >
-              <StepButton color="inherit" onClick={handleStep(index)} sx={{}}>
+          <Tooltip title={label.question} key={label.question}>
+            <Step completed={completed[index]} sx={{ m: 1 }}>
+              <StepButton color="inherit" onClick={handleStep(index)}>
                 <Typography
                   sx={{
                     overflow: "hidden",
                     textOverflow: "ellipsis",
                     whiteSpace: "nowrap",
                     height: "20px",
-                    width: "50px",
+                    maxWidth: "100px",
                   }}
                 >
                   {label.question}
@@ -100,54 +47,6 @@ export default function QuizStepper({
           </Tooltip>
         ))}
       </Stepper>
-      <div>
-        {allStepsCompleted() ? (
-          <React.Fragment>
-            <Typography sx={{ mt: 2, mb: 1 }}>
-              All steps completed - you&apos;re finished
-            </Typography>
-            <Box>
-              <Box />
-              <Button onClick={handleReset}>Reset</Button>
-            </Box>
-          </React.Fragment>
-        ) : (
-          <React.Fragment>
-            <Typography sx={{ mt: 2, mb: 1, py: 1 }}>
-              Step {activeStep + 1}
-            </Typography>
-            <Box>
-              <Button
-                color="inherit"
-                disabled={activeStep === 0}
-                onClick={handleBack}
-                sx={{ mr: 1 }}
-              >
-                Back
-              </Button>
-              <Box />
-              <Button onClick={handleNext} sx={{ mr: 1 }}>
-                Next
-              </Button>
-              {activeStep !== steps.length &&
-                (completed[activeStep] ? (
-                  <Typography
-                    variant="caption"
-                    sx={{ display: "inline-block" }}
-                  >
-                    Step {activeStep + 1} already completed
-                  </Typography>
-                ) : (
-                  <Button onClick={handleComplete}>
-                    {completedSteps() === totalSteps() - 1
-                      ? "Finish"
-                      : "Complete Step"}
-                  </Button>
-                ))}
-            </Box>
-          </React.Fragment>
-        )}
-      </div>
     </Box>
   );
 }
