cc339ca8f110dfa701ff08dba88afa70d45732f7
Marcus Mohns <marcus@mohns.se>
2024-11-27 19:03:20 +0100

    remove temp QuizData

--- src/Quizzer.jsx
+++ src/Quizzer.jsx
@@ -3,36 +3,15 @@ import Box from "@mui/material/Box";
 import QuizStepper from "./QuizStepper";
 import QuizControls from "./QuizControls";
 import QuizQuestion from "./QuizQuestion.jsx";
+import PropTypes from "prop-types";
 
-const Quizzer = () => {
-  // add quizData
-  // const [step, setStep] = useState(0);
+const Quizzer = ({ quizData }) => {
+  Quizzer.propTypes = {
+    quizData: PropTypes.array.isRequired,
+  };
 
   const [activeStep, setActiveStep] = useState(0);
   const [completed, setCompleted] = useState({});
-
-  const quizData = [
-    {
-      category: "history",
-      id: "622a1c3c7cc59eab6f951ae9",
-      correctAnswer: "1066",
-      incorrectAnswers: ["1166", "1266", "1366"],
-      question: {
-        text: "In Which Year Did The Battle Of Hastings Take Place?",
-      },
-      tags: ["battles", "uk", "history"],
-      type: "text_choice",
-      difficulty: "medium",
-      regions: [],
-      isNiche: false,
-    },
-  ];
-
-  // Sort array alphabetically
-
-  // Shuffle the questions so the correct answer isn't always the last alternative.
-
-  // Modify quizData so it contains correct answers etc and add it to a state ... Keep answers in a seperate state. ok
 
   return (
     <Box
