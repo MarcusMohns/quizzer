265c287995855e80c0cabd191f07dc37eb3b4002
Marcus Mohns <marcus@mohns.se>
2024-12-14 01:47:35 +0100

    remove temp data remove container padding

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import CssBaseline from "@mui/material/CssBaseline";
-import Container from "@mui/material/Container";
 import Quizzer from "./Quizzer";
 import Navbar from "./Navbar";
 import { useState } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import { darkTheme, lightTheme } from "./Utils/Themes";
+import Box from "@mui/material/Box";
 
 // Fetch darkMode settings from localStorage if they exist
 const darkModeData =
@@ -18,78 +18,79 @@ const darkModeData =
 
 function App() {
   const [darkMode, setDarkMode] = useState(darkModeData);
-  const [quizData, setQuizData] = useState([
-    {
-      category: "history",
-      id: "65056f0c7a97013de78b53bc",
-      correctAnswer: "Russia",
-      incorrectAnswers: ["Japan", "France", "Canada"],
-      question: {
-        text: "Which country was suspended from the G8 in 2014?",
-      },
-      tags: ["history", "2010's", "organisations", "organizations"],
-      type: "text_choice",
-      difficulty: "medium",
-      regions: [],
-      isNiche: false,
-    },
-    {
-      category: "history",
-      id: "647edb8d520523194910c347",
-      correctAnswer: "1929",
-      incorrectAnswers: ["1987", "2001", "2008"],
-      question: {
-        text: "In what year did the Dow Jones Industrial Average crash and lose 23% of its value in what is known as 'Black Tuesday'?",
-      },
-      tags: ["economics", "events", "history"],
-      type: "text_choice",
-      difficulty: "medium",
-      regions: [],
-      isNiche: false,
-    },
-    {
-      category: "history",
-      id: "622a1c3c7cc59eab6f951afe",
-      correctAnswer: "Salem",
-      incorrectAnswers: ["Greensboro", "Alexandria", "Wilmington"],
-      question: {
-        text: "In which Massachusetts town were 20 people executed for witchcraft in 1692?",
-      },
-      tags: ["1600's", "usa", "halloween", "history"],
-      type: "text_choice",
-      difficulty: "medium",
-      regions: [],
-      isNiche: false,
-    },
-    {
-      category: "history",
-      id: "622a1c357cc59eab6f94fd41",
-      correctAnswer: "Constantine",
-      incorrectAnswers: ["Byzantine", "Marcus Aurelius", "Trajan"],
-      question: {
-        text: "Which Roman Emperor moved the capital from Rome to the city now known as Istanbul? ",
-      },
-      tags: ["romans", "classics", "history"],
-      type: "text_choice",
-      difficulty: "medium",
-      regions: [],
-      isNiche: false,
-    },
-    {
-      category: "history",
-      id: "622a1c367cc59eab6f950358",
-      correctAnswer: "John Adams",
-      incorrectAnswers: ["Thomas Jefferson", "Andrew Jackson", "James Monroe"],
-      question: {
-        text: "Who was George Washington's vice-president?",
-      },
-      tags: ["history"],
-      type: "text_choice",
-      difficulty: "medium",
-      regions: [],
-      isNiche: false,
-    },
-  ]);
+  const [quizData, setQuizData] = useState([]);
+  // const [quizData, setQuizData] = useState([
+  //   {
+  //     category: "history",
+  //     id: "65056f0c7a97013de78b53bc",
+  //     correctAnswer: "Russia",
+  //     incorrectAnswers: ["Japan", "France", "Canada"],
+  //     question: {
+  //       text: "Which country was suspended from the G8 in 2014?",
+  //     },
+  //     tags: ["history", "2010's", "organisations", "organizations"],
+  //     type: "text_choice",
+  //     difficulty: "medium",
+  //     regions: [],
+  //     isNiche: false,
+  //   },
+  //   {
+  //     category: "history",
+  //     id: "647edb8d520523194910c347",
+  //     correctAnswer: "1929",
+  //     incorrectAnswers: ["1987", "2001", "2008"],
+  //     question: {
+  //       text: "In what year did the Dow Jones Industrial Average crash and lose 23% of its value in what is known as 'Black Tuesday'?",
+  //     },
+  //     tags: ["economics", "events", "history"],
+  //     type: "text_choice",
+  //     difficulty: "medium",
+  //     regions: [],
+  //     isNiche: false,
+  //   },
+  //   {
+  //     category: "history",
+  //     id: "622a1c3c7cc59eab6f951afe",
+  //     correctAnswer: "Salem",
+  //     incorrectAnswers: ["Greensboro", "Alexandria", "Wilmington"],
+  //     question: {
+  //       text: "In which Massachusetts town were 20 people executed for witchcraft in 1692?",
+  //     },
+  //     tags: ["1600's", "usa", "halloween", "history"],
+  //     type: "text_choice",
+  //     difficulty: "medium",
+  //     regions: [],
+  //     isNiche: false,
+  //   },
+  //   {
+  //     category: "history",
+  //     id: "622a1c357cc59eab6f94fd41",
+  //     correctAnswer: "Constantine",
+  //     incorrectAnswers: ["Byzantine", "Marcus Aurelius", "Trajan"],
+  //     question: {
+  //       text: "Which Roman Emperor moved the capital from Rome to the city now known as Istanbul? ",
+  //     },
+  //     tags: ["romans", "classics", "history"],
+  //     type: "text_choice",
+  //     difficulty: "medium",
+  //     regions: [],
+  //     isNiche: false,
+  //   },
+  //   {
+  //     category: "history",
+  //     id: "622a1c367cc59eab6f950358",
+  //     correctAnswer: "John Adams",
+  //     incorrectAnswers: ["Thomas Jefferson", "Andrew Jackson", "James Monroe"],
+  //     question: {
+  //       text: "Who was George Washington's vice-president?",
+  //     },
+  //     tags: ["history"],
+  //     type: "text_choice",
+  //     difficulty: "medium",
+  //     regions: [],
+  //     isNiche: false,
+  //   },
+  // ]);
 
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
@@ -99,9 +100,9 @@ function App() {
         setDarkMode={setDarkMode}
         setQuizData={setQuizData}
       />
-      <Container sx={{ height: "90vh" }}>
+      <Box sx={{ height: "90vh", p: 0 }}>
         <Quizzer quizData={quizData} />
-      </Container>
+      </Box>
     </ThemeProvider>
   );
 }
