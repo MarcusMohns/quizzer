af6db622fa56a4da59458f75ca67084af5b6975b
Marcus Mohns <marcus@mohns.se>
2024-12-17 13:25:32 +0100

    add back category in qustions array

--- src/Utils/preparedQuizzes.js
+++ src/Utils/preparedQuizzes.js
@@ -5,6 +5,7 @@ export const preparedQuizzes = [
     image: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
     questions: [
       {
+        category: "history",
         id: "65056f0c7a97013de78b53bc",
         correctAnswer: "Russia",
         incorrectAnswers: ["Japan", "France", "Canada"],
@@ -18,6 +19,7 @@ export const preparedQuizzes = [
         isNiche: false,
       },
       {
+        category: "history",
         id: "647edb8d520523194910c347",
         correctAnswer: "1929",
         incorrectAnswers: ["1987", "2001", "2008"],
@@ -31,6 +33,7 @@ export const preparedQuizzes = [
         isNiche: false,
       },
       {
+        category: "history",
         id: "622a1c3c7cc59eab6f951afe",
         correctAnswer: "Salem",
         incorrectAnswers: ["Greensboro", "Alexandria", "Wilmington"],
@@ -44,6 +47,7 @@ export const preparedQuizzes = [
         isNiche: false,
       },
       {
+        category: "history",
         id: "622a1c357cc59eab6f94fd41",
         correctAnswer: "Constantine",
         incorrectAnswers: ["Byzantine", "Marcus Aurelius", "Trajan"],
@@ -57,6 +61,7 @@ export const preparedQuizzes = [
         isNiche: false,
       },
       {
+        category: "history",
         id: "622a1c367cc59eab6f950358",
         correctAnswer: "John Adams",
         incorrectAnswers: [
@@ -81,6 +86,7 @@ export const preparedQuizzes = [
     image: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
     questions: [
       {
+        category: "history",
         id: "65056f0c7a97013de78b53bc",
         correctAnswer: "Russia",
         incorrectAnswers: ["Japan", "France", "Canada"],
@@ -94,6 +100,7 @@ export const preparedQuizzes = [
         isNiche: false,
       },
       {
+        category: "history",
         id: "647edb8d520523194910c347",
         correctAnswer: "1929",
         incorrectAnswers: ["1987", "2001", "2008"],
@@ -107,6 +114,7 @@ export const preparedQuizzes = [
         isNiche: false,
       },
       {
+        category: "history",
         id: "622a1c3c7cc59eab6f951afe",
         correctAnswer: "Salem",
         incorrectAnswers: ["Greensboro", "Alexandria", "Wilmington"],
@@ -120,6 +128,7 @@ export const preparedQuizzes = [
         isNiche: false,
       },
       {
+        category: "history",
         id: "622a1c357cc59eab6f94fd41",
         correctAnswer: "Constantine",
         incorrectAnswers: ["Byzantine", "Marcus Aurelius", "Trajan"],
@@ -133,6 +142,7 @@ export const preparedQuizzes = [
         isNiche: false,
       },
       {
+        category: "history",
         id: "622a1c367cc59eab6f950358",
         correctAnswer: "John Adams",
         incorrectAnswers: [
@@ -157,6 +167,7 @@ export const preparedQuizzes = [
     image: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
     questions: [
       {
+        category: "history",
         id: "65056f0c7a97013de78b53bc",
         correctAnswer: "Russia",
         incorrectAnswers: ["Japan", "France", "Canada"],
@@ -170,6 +181,7 @@ export const preparedQuizzes = [
         isNiche: false,
       },
       {
+        category: "history",
         id: "647edb8d520523194910c347",
         correctAnswer: "1929",
         incorrectAnswers: ["1987", "2001", "2008"],
@@ -183,6 +195,7 @@ export const preparedQuizzes = [
         isNiche: false,
       },
       {
+        category: "history",
         id: "622a1c3c7cc59eab6f951afe",
         correctAnswer: "Salem",
         incorrectAnswers: ["Greensboro", "Alexandria", "Wilmington"],
@@ -196,6 +209,7 @@ export const preparedQuizzes = [
         isNiche: false,
       },
       {
+        category: "history",
         id: "622a1c357cc59eab6f94fd41",
         correctAnswer: "Constantine",
         incorrectAnswers: ["Byzantine", "Marcus Aurelius", "Trajan"],
@@ -209,6 +223,7 @@ export const preparedQuizzes = [
         isNiche: false,
       },
       {
+        category: "history",
         id: "622a1c367cc59eab6f950358",
         correctAnswer: "John Adams",
         incorrectAnswers: [
@@ -233,377 +248,13 @@ export const preparedQuizzes = [
     image: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
     questions: [
       {
+        category: "history",
         id: "65056f0c7a97013de78b53bc",
         correctAnswer: "Russia",
         incorrectAnswers: ["Japan", "France", "Canada"],
         question: {
           text: "Which country was suspended from the G8 in 2014?",
         },
-        tags: ["history", "2010's", "organisations", "organizations"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-      {
-        id: "647edb8d520523194910c347",
-        correctAnswer: "1929",
-        incorrectAnswers: ["1987", "2001", "2008"],
-        question: {
-          text: "In what year did the Dow Jones Industrial Average crash and lose 23% of its value in what is known as 'Black Tuesday'?",
-        },
-        tags: ["economics", "events", "history"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-      {
-        id: "622a1c3c7cc59eab6f951afe",
-        correctAnswer: "Salem",
-        incorrectAnswers: ["Greensboro", "Alexandria", "Wilmington"],
-        question: {
-          text: "In which Massachusetts town were 20 people executed for witchcraft in 1692?",
-        },
-        tags: ["1600's", "usa", "halloween", "history"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-      {
-        id: "622a1c357cc59eab6f94fd41",
-        correctAnswer: "Constantine",
-        incorrectAnswers: ["Byzantine", "Marcus Aurelius", "Trajan"],
-        question: {
-          text: "Which Roman Emperor moved the capital from Rome to the city now known as Istanbul? ",
-        },
-        tags: ["romans", "classics", "history"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-      {
-        id: "622a1c367cc59eab6f950358",
-        correctAnswer: "John Adams",
-        incorrectAnswers: [
-          "Thomas Jefferson",
-          "Andrew Jackson",
-          "James Monroe",
-        ],
-        question: {
-          text: "Who was George Washington's vice-president?",
-        },
-        tags: ["history"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-    ],
-  },
-  {
-    category: "History",
-    text: "History Quiz lorem ipsumasdads",
-    image: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
-    questions: [
-      {
-        id: "65056f0c7a97013de78b53bc",
-        correctAnswer: "Russia",
-        incorrectAnswers: ["Japan", "France", "Canada"],
-        question: {
-          text: "Which country was suspended from the G8 in 2014?",
-        },
-        tags: ["history", "2010's", "organisations", "organizations"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-      {
-        id: "647edb8d520523194910c347",
-        correctAnswer: "1929",
-        incorrectAnswers: ["1987", "2001", "2008"],
-        question: {
-          text: "In what year did the Dow Jones Industrial Average crash and lose 23% of its value in what is known as 'Black Tuesday'?",
-        },
-        tags: ["economics", "events", "history"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-      {
-        id: "622a1c3c7cc59eab6f951afe",
-        correctAnswer: "Salem",
-        incorrectAnswers: ["Greensboro", "Alexandria", "Wilmington"],
-        question: {
-          text: "In which Massachusetts town were 20 people executed for witchcraft in 1692?",
-        },
-        tags: ["1600's", "usa", "halloween", "history"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-      {
-        id: "622a1c357cc59eab6f94fd41",
-        correctAnswer: "Constantine",
-        incorrectAnswers: ["Byzantine", "Marcus Aurelius", "Trajan"],
-        question: {
-          text: "Which Roman Emperor moved the capital from Rome to the city now known as Istanbul? ",
-        },
-        tags: ["romans", "classics", "history"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-      {
-        id: "622a1c367cc59eab6f950358",
-        correctAnswer: "John Adams",
-        incorrectAnswers: [
-          "Thomas Jefferson",
-          "Andrew Jackson",
-          "James Monroe",
-        ],
-        question: {
-          text: "Who was George Washington's vice-president?",
-        },
-        tags: ["history"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-    ],
-  },
-  {
-    category: "History",
-    text: "History Quiz lorem ipsumasdads",
-    image: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
-    questions: [
-      {
-        id: "65056f0c7a97013de78b53bc",
-        correctAnswer: "Russia",
-        incorrectAnswers: ["Japan", "France", "Canada"],
-        question: {
-          text: "Which country was suspended from the G8 in 2014?",
-        },
-        tags: ["history", "2010's", "organisations", "organizations"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-      {
-        id: "647edb8d520523194910c347",
-        correctAnswer: "1929",
-        incorrectAnswers: ["1987", "2001", "2008"],
-        question: {
-          text: "In what year did the Dow Jones Industrial Average crash and lose 23% of its value in what is known as 'Black Tuesday'?",
-        },
-        tags: ["economics", "events", "history"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-      {
-        id: "622a1c3c7cc59eab6f951afe",
-        correctAnswer: "Salem",
-        incorrectAnswers: ["Greensboro", "Alexandria", "Wilmington"],
-        question: {
-          text: "In which Massachusetts town were 20 people executed for witchcraft in 1692?",
-        },
-        tags: ["1600's", "usa", "halloween", "history"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-      {
-        id: "622a1c357cc59eab6f94fd41",
-        correctAnswer: "Constantine",
-        incorrectAnswers: ["Byzantine", "Marcus Aurelius", "Trajan"],
-        question: {
-          text: "Which Roman Emperor moved the capital from Rome to the city now known as Istanbul? ",
-        },
-        tags: ["romans", "classics", "history"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-      {
-        id: "622a1c367cc59eab6f950358",
-        correctAnswer: "John Adams",
-        incorrectAnswers: [
-          "Thomas Jefferson",
-          "Andrew Jackson",
-          "James Monroe",
-        ],
-        question: {
-          text: "Who was George Washington's vice-president?",
-        },
-        tags: ["history"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-    ],
-  },
-  {
-    category: "History",
-    text: "History Quiz lorem ipsumasdads",
-    image: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
-    questions: [
-      {
-        id: "65056f0c7a97013de78b53bc",
-        correctAnswer: "Russia",
-        incorrectAnswers: ["Japan", "France", "Canada"],
-        question: {
-          text: "Which country was suspended from the G8 in 2014?",
-        },
-        tags: ["history", "2010's", "organisations", "organizations"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-      {
-        id: "647edb8d520523194910c347",
-        correctAnswer: "1929",
-        incorrectAnswers: ["1987", "2001", "2008"],
-        question: {
-          text: "In what year did the Dow Jones Industrial Average crash and lose 23% of its value in what is known as 'Black Tuesday'?",
-        },
-        tags: ["economics", "events", "history"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-      {
-        id: "622a1c3c7cc59eab6f951afe",
-        correctAnswer: "Salem",
-        incorrectAnswers: ["Greensboro", "Alexandria", "Wilmington"],
-        question: {
-          text: "In which Massachusetts town were 20 people executed for witchcraft in 1692?",
-        },
-        tags: ["1600's", "usa", "halloween", "history"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-      {
-        id: "622a1c357cc59eab6f94fd41",
-        correctAnswer: "Constantine",
-        incorrectAnswers: ["Byzantine", "Marcus Aurelius", "Trajan"],
-        question: {
-          text: "Which Roman Emperor moved the capital from Rome to the city now known as Istanbul? ",
-        },
-        tags: ["romans", "classics", "history"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-      {
-        id: "622a1c367cc59eab6f950358",
-        correctAnswer: "John Adams",
-        incorrectAnswers: [
-          "Thomas Jefferson",
-          "Andrew Jackson",
-          "James Monroe",
-        ],
-        question: {
-          text: "Who was George Washington's vice-president?",
-        },
-        tags: ["history"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-    ],
-  },
-  {
-    category: "History",
-    text: "History Quiz lorem ipsumasdads",
-    image: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
-    questions: [
-      {
-        id: "65056f0c7a97013de78b53bc",
-        correctAnswer: "Russia",
-        incorrectAnswers: ["Japan", "France", "Canada"],
-        question: {
-          text: "Which country was suspended from the G8 in 2014?",
-        },
-        tags: ["history", "2010's", "organisations", "organizations"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-      {
-        id: "647edb8d520523194910c347",
-        correctAnswer: "1929",
-        incorrectAnswers: ["1987", "2001", "2008"],
-        question: {
-          text: "In what year did the Dow Jones Industrial Average crash and lose 23% of its value in what is known as 'Black Tuesday'?",
-        },
-        tags: ["economics", "events", "history"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-      {
-        id: "622a1c3c7cc59eab6f951afe",
-        correctAnswer: "Salem",
-        incorrectAnswers: ["Greensboro", "Alexandria", "Wilmington"],
-        question: {
-          text: "In which Massachusetts town were 20 people executed for witchcraft in 1692?",
-        },
-        tags: ["1600's", "usa", "halloween", "history"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-      {
-        id: "622a1c357cc59eab6f94fd41",
-        correctAnswer: "Constantine",
-        incorrectAnswers: ["Byzantine", "Marcus Aurelius", "Trajan"],
-        question: {
-          text: "Which Roman Emperor moved the capital from Rome to the city now known as Istanbul? ",
-        },
-        tags: ["romans", "classics", "history"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
-      },
-      {
-        id: "622a1c367cc59eab6f950358",
-        correctAnswer: "John Adams",
-        incorrectAnswers: [
-          "Thomas Jefferson",
-          "Andrew Jackson",
-          "James Monroe",
-        ],
-        question: {
-          text: "Who was George Washington's vice-president?",
-        },
-        tags: ["history"],
-        type: "text_choice",
-        difficulty: "medium",
-        regions: [],
-        isNiche: false,
       },
     ],
   },
