f71345498490af54c5764e726e1a3930b5bf30db
Marcus Mohns <marcus@mohns.se>
2024-12-17 18:07:02 +0100

    add front page button to open sidemenu

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,18 +6,30 @@ import ThemeToggler from "./Components/ThemeToggler";
 import SideMenu from "./SideMenu";
 import PropTypes from "prop-types";
 
-export default function Navbar({ darkMode, setDarkMode, setQuizData }) {
+export default function Navbar({
+  darkMode,
+  setDarkMode,
+  setQuizData,
+  openSideMenu,
+  setOpenSideMenu,
+}) {
   Navbar.propTypes = {
     darkMode: PropTypes.bool.isRequired,
     setDarkMode: PropTypes.func.isRequired,
     setQuizData: PropTypes.func.isRequired,
+    openSideMenu: PropTypes.bool.isRequired,
+    setOpenSideMenu: PropTypes.func.isRequired,
   };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <SideMenu setQuizData={setQuizData} />
+          <SideMenu
+            setQuizData={setQuizData}
+            openSideMenu={openSideMenu}
+            setOpenSideMenu={setOpenSideMenu}
+          />
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Generate a Quiz!
           </Typography>
