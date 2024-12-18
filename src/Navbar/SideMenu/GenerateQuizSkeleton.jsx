704d48d50ba77f79d7629a0e44ce3c0d07242a9f
Marcus Mohns <marcus@mohns.se>
2024-12-18 18:34:45 +0100

    style skeleton to match quizform component

--- src/Navbar/SideMenu/GenerateQuizSkeleton.jsx
+++ src/Navbar/SideMenu/GenerateQuizSkeleton.jsx
@@ -4,18 +4,35 @@ import Stack from "@mui/material/Stack";
 const SkeletonCheckBoxes = (len) =>
   Array.from({ length: len }, (_, index) => (
     <Stack spacing={1} sx={{ mt: 3 }} direction="row" key={`${len}-${index}`}>
-      <Skeleton variant="squared" width={30} height={30} />
-      <Skeleton variant="rounded" width={170} height={30} />
+      <Skeleton variant="squared" animation="wave" width={30} height={30} />
+      <Skeleton variant="rounded" animation="wave" width={170} height={30} />
     </Stack>
   ));
 
 export default function GenerateQuizSkeleton() {
   return (
     <Stack sx={{ px: 3, width: "280px", alignItems: "flex-start" }}>
-      <Skeleton variant="rounded" width={200} height={60} sx={{ mt: 1 }} />
+      <Skeleton
+        variant="rounded"
+        animation="wave"
+        width={230}
+        height={60}
+        sx={{ mt: 1 }}
+      />
       {SkeletonCheckBoxes(7)}
-      <Skeleton variant="text" sx={{ fontSize: "2rem", width: "90%", mt: 2 }} />
+      <Skeleton
+        variant="text"
+        animation="wave"
+        sx={{ fontSize: "2rem", width: "90%", mt: 2 }}
+      />
       {SkeletonCheckBoxes(4)}
+      <Skeleton
+        variant="rounded"
+        animation="wave"
+        width={230}
+        height={30}
+        sx={{ mt: 3 }}
+      />
     </Stack>
   );
 }
