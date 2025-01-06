4ce7013db79ce7d2ab107c734a201e046b064bab
Marcus Mohns <marcus@mohns.se>
2025-01-06 08:34:10 +0100

    Skeleton

--- src/Quizzer/QuizzerSkeleton.jsx
+++ src/Quizzer/QuizzerSkeleton.jsx
@@ -1,34 +1,30 @@
 import Skeleton from "@mui/material/Skeleton";
+import Box from "@mui/material/Box";
 
 const QuizzerSkeleton = () => {
   return (
-    <>
+    <Box sx={{ mx: { sm: "5%", lg: "20%" } }}>
       <Skeleton
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          height: "600px",
-          width: "100%",
+          height: "400px",
           alignItems: "center",
-          p: 0,
           borderRadius: "7px",
         }}
       />
+      <Skeleton variant="text" sx={{ mx: "10%", fontSize: "2rem" }} />
       <Skeleton
         variant="outlined"
         sx={{ alignSelf: "flex-start", mx: "10%", my: 2 }}
       />
       <Skeleton
         sx={{
-          display: "flex",
-          flexDirection: "column",
           alignItems: "center",
           mt: { xs: 2, md: 5 },
           borderRadius: "10px",
           px: { sm: "5%", lg: "20%" },
         }}
       ></Skeleton>
-    </>
+    </Box>
   );
 };
 
