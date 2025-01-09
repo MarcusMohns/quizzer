471801bfe03ea783bff20cf38dc07de799cbdc05
Marcus Mohns <marcus@mohns.se>
2025-01-09 15:16:02 +0100

    Animate some components in result & add % text

--- src/Quizzer/Results.jsx
+++ src/Quizzer/Results.jsx
@@ -3,11 +3,13 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
 import tags from "../Utils/tags";
-
+import Grow from "@mui/material/Grow";
+import Zoom from "@mui/material/Zoom";
+import Fade from "@mui/material/Fade";
 import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
@@ -56,15 +58,28 @@ const Results = ({ results, quizData, totalQuestions }) => {
   ];
 
   return (
-    <>
-      <Typography
-        id=""
-        variant="h6"
-        component="h2"
-        sx={{ mt: 5, textAlign: "center" }}
-      >
-        {correctAnswers} correct out of {totalQuestions} total questions!
-      </Typography>
+    <Box>
+      <Zoom in={true} appear={true} timeout={300}>
+        <Typography
+          id=""
+          variant="h4"
+          component="h2"
+          sx={{ mt: 5, textAlign: "center" }}
+        >
+          {(correctAnswers / totalQuestions) * 100}% Correct!
+        </Typography>
+      </Zoom>
+      <Grow in={true} appear={true} timeout={900}>
+        <Typography
+          id=""
+          variant="h6"
+          component="h3"
+          sx={{ mt: 1, textAlign: "center" }}
+        >
+          {correctAnswers} correct out of {totalQuestions} total questions!
+        </Typography>
+      </Grow>
+
       <Stack
         direction="row"
         spacing={1}
@@ -81,53 +96,55 @@ const Results = ({ results, quizData, totalQuestions }) => {
           />
         ))}
       </Stack>
-      <List dense={true}>
-        {resultsArray.map((result, index) =>
-          result.correctlyAnswered === true ? (
-            <ListItem
-              key={index}
-              sx={{ backgroundColor: "success.default", my: 1 }}
-            >
-              <ListItemIcon>{result.questionNum}</ListItemIcon>
-              <ListItemText
-                primary={result.question.text}
-                secondary={`Correct!  the answer was: ${result.correctAnswer}`}
-              />
-              <ListItemIcon>
-                <CheckCircleOutlineIcon />
-              </ListItemIcon>
-            </ListItem>
-          ) : result.pickedAnswer === "None" ? (
-            <ListItem key={index} sx={{ backgroundColor: "#666", my: 1 }}>
-              <ListItemIcon>{result.questionNum}</ListItemIcon>
-              <ListItemText
-                primary={result.question.text}
-                secondary={`You have yet to pick an answer.`}
-              />
-              <ListItemIcon>
-                <HelpOutlineOutlinedIcon />
-              </ListItemIcon>
-            </ListItem>
-          ) : (
-            <ListItem
-              key={index}
-              sx={{ backgroundColor: "error.default", my: 1 }}
-            >
-              <ListItemIcon>{result.questionNum}</ListItemIcon>
-              <ListItemText
-                primary={result.question.text}
-                secondary={`Incorrect - You picked ${
-                  result.sortedAnswers[result.pickedAnswer]
-                }, the correct answer was: ${result.correctAnswer}`}
-              />
-              <ListItemIcon>
-                <CancelOutlinedIcon />
-              </ListItemIcon>
-            </ListItem>
-          )
-        )}
-      </List>
-    </>
+      <Fade in={true} appear={true} timeout={700}>
+        <List dense={true}>
+          {resultsArray.map((result, index) =>
+            result.correctlyAnswered === true ? (
+              <ListItem
+                key={index}
+                sx={{ backgroundColor: "success.default", my: 1 }}
+              >
+                <ListItemIcon>{result.questionNum}</ListItemIcon>
+                <ListItemText
+                  primary={result.question.text}
+                  secondary={`Correct!  the answer was: ${result.correctAnswer}`}
+                />
+                <ListItemIcon>
+                  <CheckCircleOutlineIcon />
+                </ListItemIcon>
+              </ListItem>
+            ) : result.pickedAnswer === "None" ? (
+              <ListItem key={index} sx={{ backgroundColor: "#666", my: 1 }}>
+                <ListItemIcon>{result.questionNum}</ListItemIcon>
+                <ListItemText
+                  primary={result.question.text}
+                  secondary={`You have yet to pick an answer.`}
+                />
+                <ListItemIcon>
+                  <HelpOutlineOutlinedIcon />
+                </ListItemIcon>
+              </ListItem>
+            ) : (
+              <ListItem
+                key={index}
+                sx={{ backgroundColor: "error.default", my: 1 }}
+              >
+                <ListItemIcon>{result.questionNum}</ListItemIcon>
+                <ListItemText
+                  primary={result.question.text}
+                  secondary={`Incorrect - You picked ${
+                    result.sortedAnswers[result.pickedAnswer]
+                  }, the correct answer was: ${result.correctAnswer}`}
+                />
+                <ListItemIcon>
+                  <CancelOutlinedIcon />
+                </ListItemIcon>
+              </ListItem>
+            )
+          )}
+        </List>
+      </Fade>
+    </Box>
   );
 };
 export default Results;
