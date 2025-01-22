573124dc13ceb7072c3247c92db3e9e0eaa9a47a
Marcus Mohns <marcus@mohns.se>
2025-01-22 17:48:47 +0100

    add functionality to back btn

--- src/Quizzer/Components/StartPage/StartPage.jsx
+++ src/Quizzer/Components/StartPage/StartPage.jsx
@@ -5,34 +5,50 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import QuizInfo from "./Components/QuizInfo.jsx";
 import SelectTimer from "./Components/SelectTimer.jsx";
+import Fade from "@mui/material/Fade";
 
-const StartPage = ({ timeLimit, setTimeLimit, setQuizStarted, quizData }) => {
+const StartPage = ({
+  timeLimit,
+  setTimeLimit,
+  setQuizState,
+  quizData,
+  quizState,
+  resetQuizData,
+}) => {
   return (
-    <Box
-      component="section"
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        minHeight: "600px",
-        height: "100%",
-        width: "100%",
-        alignItems: "center",
-        p: 5,
-      }}
-    >
-      <Typography variant="h5" component="h3">
-        About to start the quiz!
-        {quizData.length} questions!
-      </Typography>
-      <SelectTimer timeLimit={timeLimit} setTimeLimit={setTimeLimit} />
-      <QuizInfo quizData={quizData} />
-      <Stack spacing={2} direction="row">
-        <Button variant="outlined">Back</Button>
-        <Button variant="contained" onClick={() => setQuizStarted(true)}>
-          Start Quiz!
-        </Button>
-      </Stack>
-    </Box>
+    <Fade in={true} timeout={1000}>
+      <Box
+        component="section"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "600px",
+          height: "100%",
+          width: "100%",
+          alignItems: "center",
+          p: 5,
+        }}
+      >
+        <Typography variant="h5" component="h3">
+          About to start the quiz!
+          {quizData.length} questions!
+        </Typography>
+        <SelectTimer timeLimit={timeLimit} setTimeLimit={setTimeLimit} />
+        <QuizInfo quizData={quizData} />
+        <Stack spacing={2} direction="row">
+          <Button variant="outlined" onClick={resetQuizData}>
+            Back
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() => setQuizState({ ...quizState, started: true })}
+            disabled={timeLimit.minutes === 0 && timeLimit.seconds === 0}
+          >
+            Start Quiz!
+          </Button>
+        </Stack>
+      </Box>
+    </Fade>
   );
 };
 
