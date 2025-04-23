f656cd19d8d60fc7ab5dd0bfa8aaa1b385affa42
Marcus Mohns <marcus@mohns.se>
2025-04-23 16:47:27 +0200

    fix dark mode so it uses system preferences as well

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,52 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "./App.css";
 import CssBaseline from "@mui/material/CssBaseline";
-import { useState } from "react";
+import { useCallback, useState, useMemo } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import { darkTheme, lightTheme } from "./Themes.js";
 import Navbar from "./Navbar/Navbar.jsx";
 import Quizzer from "./Quizzer/Quizzer.tsx";
-import "./App.css";
-import { useCallback, useMemo } from "react";
 import FrontPage from "./Frontpage/FrontPage.js";
-import { QuizState } from "./store.js";
 import Footer from "./Footer.tsx";
-// Fetch darkMode settings from localStorage if they exist
-
-// const darkModeData =
-//   localStorage.getItem("Darkmode") !== null
-//     ? JSON.parse(localStorage.getItem("Darkmode"))
-//     : true;
+import { QuizState } from "./store.js";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 function App() {
-  const darkModeData = useMemo(
-    () => localStorage.getItem("Darkmode") === "true",
-    []
+  const darkModeSystemPreference = useMediaQuery(
+    "(prefers-color-scheme: dark)"
   );
+  const darkModeLocalStorage = localStorage.getItem("Darkmode");
+
+  const darkModeData = useMemo(() => {
+    if (darkModeLocalStorage === "true") {
+      // Check local storage for dark mode first
+      return true;
+    } else if (darkModeLocalStorage === "false") {
+      return false;
+      // Check for system preference
+    } else if (darkModeSystemPreference) {
+      return true;
+    } else {
+      // Default to use light mode
+      return false;
+    }
+  }, [darkModeLocalStorage, darkModeSystemPreference]);
+
   const [darkMode, setDarkMode] = useState<boolean>(darkModeData);
   const [sideMenuOpen, setSideMenuOpen] = useState<boolean>(false);
   const [quizData, setQuizData] = useState<QuizState | null>(null);
 
   const handleSetDarkMode = useCallback(() => {
-    localStorage.setItem("Darkmode", JSON.stringify(!darkMode));
+    localStorage.setItem("Darkmode", (!darkMode).toString());
     setDarkMode((oldDarkMode) => !oldDarkMode);
   }, [darkMode]);
 
   const handleSideMenuOpen = useCallback(
     (open: boolean, event?: React.SyntheticEvent<object, Event>) => {
       if (
-        // TODO figure out why they want to do this exactly
+        // Disable Tab and Shift to avoid accidentally navigating to the sidemenu
         event &&
         event.type === "keydown" &&
         ((event as React.KeyboardEvent).key === "Tab" ||
