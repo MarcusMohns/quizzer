0134cbda45dd1aa61a3770ea1641cdbf6f64f9e5
Marcus Mohns <marcus@mohns.se>
2025-11-18 23:03:13 +0100

    replace animated bg svg with css alternative for performance reasons

--- src/quizzer/Quizzer.tsx
+++ src/quizzer/Quizzer.tsx
@@ -6,12 +6,13 @@ import Results from "./components/Results.tsx";
 import ResultsModal from "./components/ResultsModal.tsx";
 import Button from "@mui/material/Button";
 import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
-import StartPageSkeleton from "./components/startPage/components/StartPageSkeleton.tsx";
+import StartPageSkeleton from "./components/start-page/components/StartPageSkeleton.tsx";
 import QuizSkeleton from "./quiz/components/QuizSkeleton.tsx";
-const StartPage = lazy(() => import("./components/startPage/StartPage.tsx"));
+const StartPage = lazy(() => import("./components/start-page/StartPage.tsx"));
 const Quiz = lazy(() => import("./quiz/Quiz.tsx"));
 import useQuizzer from "./useQuizzer.ts";
 import { QuizState } from "../store.tsx";
+import AnimatedSquares from "../css-animations/animated-squares/AnimatedSquares.tsx";
 
 interface QuizzerProps {
   quizData: QuizState;
@@ -62,18 +63,14 @@ const Quizzer = ({ quizData, handleSetQuizData }: QuizzerProps) => {
       component="section"
       sx={{
         display: "flex",
+        position: "relative",
         flexDirection: "column",
         minHeight: "600px",
         alignItems: "center",
         py: 5,
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundImage: {
-          md: "url(./images/animated-shapes.svg)",
-        },
       }}
     >
+      <AnimatedSquares />
       <Button
         variant="outlined"
         // Emptying quizData renders the FrontPage, Quiz is only rendered if quizData has data
